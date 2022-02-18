3248bd27ad2a9a894fd4ca396ac6da717fe0ea71
dylanswl <dvsewell15@gmail.com>
2022-02-17 22:00:01 -0800

    First commit project setup

---
+++ NetworkRequest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkRequest
+//
+//  Created by Dylan Sewell on 2/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
